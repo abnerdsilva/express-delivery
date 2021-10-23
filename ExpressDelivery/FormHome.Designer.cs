b8c7fb5b87776739f21dda8b5939eda1049f4a27
Abner Silva <abner.computadores@outlook.com>
2021-10-23 12:11:56 -0300

    definindo layout do FormHome e setando exemplo imagem do logo

--- ExpressDelivery/FormHome.Designer.cs
+++ ExpressDelivery/FormHome.Designer.cs
@@ -31,12 +31,191 @@ namespace ExpressDelivery
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelSideMenu = new System.Windows.Forms.Panel();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnPedido = new System.Windows.Forms.Button();
+            this.panelCadastros = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnCadastro = new System.Windows.Forms.Button();
+            this.btnDashbord = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelSideMenu.SuspendLayout();
+            this.panelCadastros.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelSideMenu
+            // 
+            this.panelSideMenu.AutoScroll = true;
+            this.panelSideMenu.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panelSideMenu.Controls.Add(this.btnSair);
+            this.panelSideMenu.Controls.Add(this.btnPedido);
+            this.panelSideMenu.Controls.Add(this.panelCadastros);
+            this.panelSideMenu.Controls.Add(this.btnCadastro);
+            this.panelSideMenu.Controls.Add(this.btnDashbord);
+            this.panelSideMenu.Controls.Add(this.panel1);
+            this.panelSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSideMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.panelSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelSideMenu.Name = "panelSideMenu";
+            this.panelSideMenu.Size = new System.Drawing.Size(200, 729);
+            this.panelSideMenu.TabIndex = 0;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.Location = new System.Drawing.Point(0, 689);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(200, 40);
+            this.btnSair.TabIndex = 6;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnPedido
+            // 
+            this.btnPedido.BackColor = System.Drawing.Color.Silver;
+            this.btnPedido.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPedido.FlatAppearance.BorderSize = 0;
+            this.btnPedido.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPedido.Location = new System.Drawing.Point(0, 326);
+            this.btnPedido.Name = "btnPedido";
+            this.btnPedido.Size = new System.Drawing.Size(200, 40);
+            this.btnPedido.TabIndex = 5;
+            this.btnPedido.Text = "Pedido";
+            this.btnPedido.UseVisualStyleBackColor = false;
+            // 
+            // panelCadastros
+            // 
+            this.panelCadastros.Controls.Add(this.button5);
+            this.panelCadastros.Controls.Add(this.button4);
+            this.panelCadastros.Controls.Add(this.button3);
+            this.panelCadastros.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelCadastros.Location = new System.Drawing.Point(0, 214);
+            this.panelCadastros.Name = "panelCadastros";
+            this.panelCadastros.Size = new System.Drawing.Size(200, 112);
+            this.panelCadastros.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(0, 66);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(200, 33);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Usuário";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(0, 33);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 33);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Cliente";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 33);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Cardapio";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // btnCadastro
+            // 
+            this.btnCadastro.BackColor = System.Drawing.Color.Silver;
+            this.btnCadastro.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCadastro.FlatAppearance.BorderSize = 0;
+            this.btnCadastro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastro.Location = new System.Drawing.Point(0, 174);
+            this.btnCadastro.Name = "btnCadastro";
+            this.btnCadastro.Size = new System.Drawing.Size(200, 40);
+            this.btnCadastro.TabIndex = 3;
+            this.btnCadastro.Text = "Cadastro";
+            this.btnCadastro.UseVisualStyleBackColor = false;
+            this.btnCadastro.Click += new System.EventHandler(this.btnCadastro_Click);
+            // 
+            // btnDashbord
+            // 
+            this.btnDashbord.BackColor = System.Drawing.Color.Silver;
+            this.btnDashbord.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashbord.FlatAppearance.BorderSize = 0;
+            this.btnDashbord.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashbord.Location = new System.Drawing.Point(0, 134);
+            this.btnDashbord.Name = "btnDashbord";
+            this.btnDashbord.Size = new System.Drawing.Size(200, 40);
+            this.btnDashbord.TabIndex = 1;
+            this.btnDashbord.Text = "Dashboard";
+            this.btnDashbord.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 134);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ExpressDelivery.Properties.Resources.no_imagem;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormHome";
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.panelSideMenu);
+            this.MinimumSize = new System.Drawing.Size(1024, 768);
+            this.Name = "FormHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Gerenciamento de Pedidos";
+            this.panelSideMenu.ResumeLayout(false);
+            this.panelCadastros.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
         }
 
+        private System.Windows.Forms.PictureBox pictureBox1;
+
         #endregion
+
+        private System.Windows.Forms.Panel panelSideMenu;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnPedido;
+        private System.Windows.Forms.Panel panelCadastros;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnCadastro;
+        private System.Windows.Forms.Button btnDashbord;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- ExpressDelivery/FormHome.cs
+++ ExpressDelivery/FormHome.cs
@@ -7,6 +7,40 @@ namespace ExpressDelivery
         public FormHome()
         {
             InitializeComponent();
+
+            CustomizeDesign();
+        }
+
+        private void CustomizeDesign()
+        {
+            panelCadastros.Visible = false;
+        }
+
+        private void HideSubMenu()
+        {
+            if (panelCadastros.Visible)
+                panelCadastros.Visible = false;
+        }
+
+        private void ShowSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                HideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+
+        private void btnSair_Click(object sender, System.EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCadastro_Click(object sender, System.EventArgs e)
+        {
+            ShowSubMenu(panelCadastros);
         }
     }
 }
