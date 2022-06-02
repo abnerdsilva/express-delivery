95ddd84d560edd7e582ec9fff7a463226a54f938
Abner Silva <abner.computadores@outlook.com>
2022-06-02 20:59:12 -0300

    add exemplo imprimir pedido no pacote delivery

--- src/main/java/delivery/AppDelivery.java
+++ src/main/java/delivery/AppDelivery.java
@@ -5,8 +5,10 @@ import delivery.model.ClienteDelivery;
 import delivery.model.PagamentoDelivery;
 import delivery.model.PedidoDelivery;
 import delivery.model.PedidoItemDelivery;
+import imprime.Impressora;
 import log.LoggerInFile;
 
+import javax.print.PrintException;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -77,5 +79,42 @@ public class AppDelivery {
             e.printStackTrace();
             LoggerInFile.printError(e.getMessage());
         }
+
+        Impressora imprimir = new Impressora();
+
+        String textToPrint = "\n-----------------------------------------------------------\n";
+        textToPrint += imprimir.preencheLinha("Pedido Delivery", " ", 60, "E");
+        textToPrint += "\n-----------------------------------------------------------";
+
+        textToPrint += "\n";
+        textToPrint += "\n";
+
+        textToPrint += imprimir.preencheLinha("VIA: 1", "*", 60, "ED");
+        textToPrint += "\n";
+        textToPrint += "\n";
+
+        textToPrint += imprimir.preencheLinha("Nome", " ", 40, "M")
+                + imprimir.preencheLinha("Qtde", " ", 7, "M")
+                + imprimir.preencheLinha("V.Unit", " ", 7, "M")
+                + imprimir.preencheLinha("V.Total", " ", 7, "M");
+//                + preencheLinha("1", "*", 6, "EDB")
+//                + preencheLinha("1540,54", "*", 10, "EDB") + "\n";
+        for (var p : pedidoDelivery.getItens()) {
+            textToPrint += imprimir.preencheLinha(p.getNome(), "-", 40, "M");
+//                + preencheLinha("1", "*", 6, "EDB")
+//                + preencheLinha("1540,54", "*", 10, "EDB") + "\n";
+        }
+
+
+        imprimir.detectaImpressoras();
+        try {
+            imprimir.imprime(textToPrint);
+            imprimir.imprime("\n");
+            imprimir.imprime("\n");
+        } catch (PrintException e) {
+            e.printStackTrace();
+            LoggerInFile.printError(e.getMessage());
+        }
+//        imprimir.imprime2();
     }
 }
