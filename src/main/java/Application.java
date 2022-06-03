d507c88ae1926e134583cc80237c7045dad0efd7
Abner Silva <abner.computadores@outlook.com>
2022-06-03 07:50:55 -0300

    verifica permissão para uso da integração e inicia projeto

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,33 @@
-public class Application {
-    public static void main(String[] args) {
+import delivery.controller.ConfigController;
+import ifood.AppIfood;
+import log.LoggerInFile;
+import log.MessageDefault;
 
+import java.sql.SQLException;
+
+public class Application {
+    private final static String itemConfigurationIfood = "INTEGRA_IFOOD";
+
+    private static final ConfigController configController = new ConfigController();
+
+    public static void main(String[] args) {
+        LoggerInFile.start();
+
+        try {
+            boolean statusIntegration = configController.checkIntegrationPermition(itemConfigurationIfood);
+            if (!statusIntegration) {
+                System.out.println(MessageDefault.msgAccessIntegrationNotGranted);
+                LoggerInFile.printInfo(MessageDefault.msgAccessIntegrationNotGranted);
+                return;
+            }
+
+            LoggerInFile.printInfo(MessageDefault.msgAccessIntegrationGranted);
+
+            AppIfood.start();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            LoggerInFile.printError(e.getMessage());
+            return;
+        }
     }
 }
